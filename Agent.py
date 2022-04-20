bbdac5f0cb201e0aaf1796223c00cdc76666c25e
Jonathan Diller <jonathan.a.diller@gmail.com>
2022-04-20 13:56:50 -0600

    Added time-step mechanism

--- Agent.py
+++ Agent.py
@@ -1,3 +1,5 @@
+import random
+
 
 class Agent:
 	# Class constructor. id is agent ID, x,y are agents initial position on grid
@@ -7,4 +9,23 @@ class Agent:
 		self.X = x
 		self.Y = y
 
+	# Well-defined position update
+	def updatePosition(self, dx, dy):
+		if self.X + dx >= 0:
+			self.X += dx
+		if self.Y + dy >= 0:
+			self.Y += dy
 
+	def update(self):
+		print("Updating agent position!")
+
+		# For now, just make random moves
+		rnd = random.random()
+		if rnd < 0.1:
+			self.updatePosition(1, 0)
+		elif rnd < 0.2:
+			self.updatePosition(-1, 0)
+		elif rnd < 0.3:
+			self.updatePosition(0, 1)
+		elif rnd < 0.4:
+			self.updatePosition(0, -1)
--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,34 @@
 import Agent
+import random
+import time
+
+
+def runTSUpdates(queue, ids):
+	print("Running Time-Step Updates")
+
+	# Randomly generate new agents
+	rnd = random.random()
+	if rnd < 0.1:
+		# Create a new agent
+		print("Creating new agent ", ids)
+		queue.append(Agent.Agent(ids))
+		ids += 1
+
+	# Run updates on each agent
+	for agent in queue:
+		agent.update()
+
+	# Update grid-world display here
+
+	time.sleep(0.5)
+	return queue, ids
 
 
 if __name__ == '__main__':
 	print("hello world!")
+	agentQueue = []
+	agentIDs = 0
+
+	# Main simulation loop, runs once per time-step
+	while True:
+		agentQueue, agentIDs = runTSUpdates(agentQueue, agentIDs)
