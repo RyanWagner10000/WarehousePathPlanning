62b6c7c5115269450287628dfe38641c10eaa929
Jonathan Diller <jonathan.a.diller@gmail.com>
2022-04-26 14:23:04 -0600

    Added nodes, updated printing

--- hello_world.py
+++ hello_world.py
@@ -1,8 +1,4 @@
-import numpy as np
-from PIL import Image
 from Map import WHMap
-import os
-import csv
 import Agent
 import random
 import time
@@ -35,43 +31,6 @@ if __name__ == '__main__':
 	agentQueue = []
 	agentIDs = 0
 	whMap = WHMap()
-
-	# black = (0, 0, 0)
-	# blue = (0, 0, 204)
-	# yellow = (255, 255, 0)
-	# red = (102, 204, 0)
-	# white = (255, 255, 255)
-	#
-	# path = os.getcwd() + "\\map.xlsx"
-	#
-	# results = []
-	# with open("map.csv") as csvfile:
-	# 	reader = csv.reader(csvfile)  # change contents to floats
-	# 	for row in reader:  # each row is a list
-	# 		results.append(row)
-	#
-	# whMap = []
-	#
-	# for r in range(0, len(results)):
-	# 	row = []
-	# 	for c in range(0, len(results[0])):
-	# 		if results[r][c] == '':
-	# 			row.append(white)
-	# 		elif results[r][c] == 'x':
-	# 			row.append(black)
-	# 		elif results[r][c] == 's':
-	# 			row.append(blue)
-	# 		elif results[r][c] == 'g':
-	# 			row.append(yellow)
-	# 		else:
-	# 			print("No color detected")
-	# 	whMap.append(row)
-	#
-	# map_array = np.array(whMap, dtype=np.uint8)
-	# map_image = Image.fromarray(map_array)
-	# map_image = map_image.resize((500, 500), resample=Image.NEAREST)
-	# map_image.save("map.png")
-	# map_image.show()
 
 	# Main simulation loop, runs once per time-step
 	while True:
